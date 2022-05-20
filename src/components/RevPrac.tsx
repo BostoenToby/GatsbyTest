e865d95eae0cb059575c04df6272ecca0e66d671
Toby <toby.bostoen@student.howest.be>
2022-05-20 14:21:32 +0200

    netlify plugin installed + transformer-sharp gone

--- src/components/RevPrac.tsx
+++ src/components/RevPrac.tsx
@@ -6,34 +6,34 @@ import { render } from 'react-dom'
 import Tag from './Tag'
 
 export default function RevPrac({image, imageAlt, leftTagText, leftTagColorBg, leftTagColorText, rightTagText, rightTagColorBg, rightTagColorText, title, subTitle}: {image: string, imageAlt: string, leftTagText: string, leftTagColorBg: string, leftTagColorText: string, rightTagText: string, rightTagColorBg: string, rightTagColorText: string, title: string, subTitle: string}) {
-  // const [img, setImg] = React.useState<any>()
-  // let node: number = 0
-  //   const data = useStaticQuery(graphql`
-  //     query HeaderQuery {
-  //       allImageSharp {
-  //           nodes {
-  //             gatsbyImageData
-  //           }
-  //       }
-  //     }
-  //   `)
-  //   React.useEffect(() => {
-  //     for(let i of data.allImageSharp.nodes){
-  //       if(i.gatsbyImageData.images.fallback.src.includes(image)){
-  //         setImg(getImage(i))
-  //       }
-  //       node += 1
-  //     }
-  //   }, [])
+  const [img, setImg] = React.useState<any>()
+  let node: number = 0
+    const data = useStaticQuery(graphql`
+      query HeaderQuery {
+        allImageSharp {
+            nodes {
+              gatsbyImageData
+            }
+        }
+      }
+    `)
+    React.useEffect(() => {
+      for(let i of data.allImageSharp.nodes){
+        if(i.gatsbyImageData.images.fallback.src.includes(image)){
+          setImg(getImage(i))
+        }
+        node += 1
+      }
+    }, [])
 
-    // React.useEffect(() => {
-    //   console.log(img)
-    // }, [img])
+    React.useEffect(() => {
+      console.log(img)
+    }, [img])
     return (
       <div>
-        {/* {img != "" && (
+        {img != "" && (
           <GatsbyImage image={img} alt="Test" />
-        )} */}
+        )}
         <div className="flex items-center relative bottom-3 left-3">
           <Tag text={leftTagText} colorBg={leftTagColorBg} colorText={leftTagColorText} />
           <Tag text={rightTagText} colorBg={rightTagColorBg} colorText={rightTagColorText}/>
