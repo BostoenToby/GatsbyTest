00e2224e66e3fbd36685c233b733c6c42ffafc1a
Toby <toby.bostoen@student.howest.be>
2022-05-20 14:01:34 +0200

    interfaces, data and gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,53 @@
 import type { GatsbyConfig } from "gatsby"
+import path from "path"
 
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Poppins', 'Droid Serif']
+        }
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, 'src', 'images')
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      }
+    },
+    {
+      resolve: `gatsby-transformer-excel`,
+      options: {
+        raw: false
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        forceBase64Format: `png`,
+        defaults: {
+          formats: ['png']
+        }
+      }
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-smoothscroll`
+  ],
 }
 
 export default config
